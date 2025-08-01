3191501bfd51cb969c42ea700a4b20c13dc935aa
oussama-dot <eloussamafouah@gmail.com>
2025-08-01 11:15:48 +0100

    Mysql

---
+++ MySQLServer.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+try :
+    mydb = mysql.connector.connect(host ="localhost",user='root', password='Oussama@97970',database="alx_book_store")
+    print("the server has been connected sucssufly")
+except mysql.connector.Error as e:
+    print(e)
+try :     
+  mycursor = mydb.cursor()
+  mycursor.execute("CREATE DATABASE alx_book_store")
+  print("Database 'alx_book_store' created successfully.")
+
+except mysql.connector.errors.DatabaseError as e :
+    print("the error is ", e)
